1d22be9d2691c07a3bb03e8393da89b8b043c431
Rafael Evangelista <rafael@mobile2you.com.br>
2020-04-01 01:43:56 -0300

    Refactoring model User spec and applying ffaker and factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is invalid if the level is not between 1 and 99' do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    hero = User.new(nickname: nickname, kind: kind, level: level)
-    expect(hero).to_not be_valid
+    user = build(:user, level: FFaker::Random.rand(100..9999))
+    expect(user).to_not be_valid
   end
 
   it 'return the correct hero title' do
@@ -16,7 +13,8 @@ RSpec.describe User, type: :model do
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
 
-    hero = User.create(nickname: nickname, kind: kind, level: level)
-    expect(hero.title).to eq("#{kind} #{nickname} ##{level}")
+    puts level
+    user = create(:user, nickname: nickname, kind: kind, level: level)
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
